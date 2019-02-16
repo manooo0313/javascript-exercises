0ac2cf5fb9bc03b7325b2c011d54c1771da99463
Manooo0313 <loth.david.mano@hotmail.com>
2019-02-17 00:23:23 +0100

    add my sulution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return ("ERROR");
+    } else if (num === 0) {
+        return ("");
+    } else {
+        let results = str;
+        for (let i = 1; i < num; i++) {
+            results = results + str;
+        } 
+        return results;
+    }
 }
 
 module.exports = repeatString
