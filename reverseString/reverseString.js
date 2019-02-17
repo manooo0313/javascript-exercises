a08c20bd96000c64cf0ee1495fca90f79ac26d5c
Manooo0313 <loth.david.mano@hotmail.com>
2019-02-17 22:05:45 +0100

    reverseStr is done

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,6 +1,11 @@
 const reverseString = function(str) {
-    
-
+    let reversebuilt = "" ;
+    let strl = str.length;
+    for (let i = 0; i < strl; i++) {
+        reversebuilt = str.charAt(0) + reversebuilt;
+        str = str.slice(1);
+    }
+    return reversebuilt; 
 }
 
 module.exports = reverseString
