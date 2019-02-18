b9d5bdc245123f8847da12da0bc2174d42734214
Manooo0313 <loth.david.mano@hotmail.com>
2019-02-18 11:38:43 +0100

    allSum done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 >= 0 && num2 >= 0) {
+            if (num1 < num2) {
+                while (num1 <= num2) {
+                    sum += num1;
+                    num1 ++;
+                } return sum;
+                
+            } else {
+                while (num2 <= num1) {
+                    sum += num2;
+                    num2 ++;
+                } return sum;
+            }
+        } else{
+            return ("ERROR");
+        }
+    } else {
+        return ("ERROR");
+    }
 }
 
 module.exports = sumAll
